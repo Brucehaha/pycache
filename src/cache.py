67c1d10d62df7dd5984c69ddaf3cf78cac385939
bruce <bruceapple@hotmail.com>
2022-02-07 12:13:44 +1100

    feature: ttl implement ### clean expired data each time cache initialized

--- src/cache.py
+++ src/cache.py
@@ -1,18 +1,22 @@
 import os
+import time
 import pickle
-
+import gevent
 from src.settings import DB_PATH
+
 
 class Cache:
     """
-    Simpale Cache System l
+    Simple Cache System l
     oad_state from binay file and 
     Save_state is to save valatile data from memory to file
     """
     volatile_data = {}
+    expiring = {}
 
     def __init__(self):
         self.load_state() # load cache from file
+        self.clean_expire()
     
     def __enter__(self):
         return self
@@ -20,7 +24,7 @@ class Cache:
     def __exit__(self, exc_type, exc_value, traceback):
         self.save_state() # save to file
 
-    def save_sate(self):
+    def save_state(self):
         pickle.dump({"volatile_data": self.volatile_data}, open(DB_PATH, 'wb'))
     
     def load_state(self):
@@ -28,8 +32,26 @@ class Cache:
             state = pickle.load(open(DB_PATH, 'rb'))
             self.volatile_data = state['volatile_data']
 
-
-         
-
-
+    def clean_expire(self):
+        """
+        clear expired item from self.expiring and self.volatile_data
+        set expiration for non expired item 
+        """
+        expired = set()
+        now = time.time()
+        for id, ttl in self.expiring.items():
+            if ttl <= now: # expired
+                expired.add(id)
+                if id in self.volatile_data:
+                    del self.volatile_data[id]
+            else: # not expired yet
+                def delete_when_expire(e, cache_obj):
+                    del cache_obj.volatile_data[e]
+                    del cache_obj.expiring[e]
+                expired_in = ttl - time.time()
+                print('expire in %s' % expired_in)
+                gevent.spawn_later(expired_in, delete_when_expire, id, self)
+        for exp in expired:
+            del self.expiring[exp]
+                    
 cache = Cache()
--- tests/test_cache.py
+++ tests/test_cache.py
@@ -1,23 +1,67 @@
 import os
+import time
+import pytest
 import tempfile
 from pathlib import Path
-
+import gevent
 from unittest.mock import patch
 
 
 from src.cache import cache
 
 
-
 tmp_path = Path(tempfile.mkdtemp()) / "snapshot.rdb"
 
-@patch('src.settings.DB_PATH',)
-def test_save_and_load_state_succeed(db_path):
-    cache.volatile_data['id'] = 1
-    cache.volatile_data['message'] = "hello world"
-    cache.save_sate()
-    assert os.path.exists(db_path)
+
+@pytest.fixture(autouse=True)
+def run_before_and_after_tests(tmpdir):
+    """Fixture to execute asserts before and after a test is run"""
+    # Setup: fill with any logic you want
+
+    yield # this is where the testing happens
+    try:
+        cache.volatile_data.clear()
+        os.remove(tmp_path)
+        print(tmp_path)
+    except OSError:
+        pass
+
+
+@patch('src.cache.DB_PATH',tmp_path)
+def test_save_and_load_state_succeed():
+    """
+    Given set key value into cache
+    When save state
+    Then data will saved to right file position and can bed loaded out sucessfully
+    """
+    cache.volatile_data['test'] = 9999999
+    cache.save_state()
+    assert os.path.exists(tmp_path)
     cache.volatile_data.clear()
     cache.load_state()
-    assert cache.volatile_data['id'] == 1
-    assert cache.volatile_data['message'] == "hello world"
+    assert isinstance(cache.volatile_data['test'], int)
+    assert cache.volatile_data['test'] == 9999999
+
+
+@patch('src.cache.DB_PATH',tmp_path)
+def test_cache_expired_and_removed_succeed():
+    """
+    Given when store data in to cache with exipred time in 1 second
+    When clean_expired after one second
+    Then data will be cleared
+    """
+    nid ='id'
+    cache.volatile_data[nid] = "test"
+    cache.expiring[nid] = time.time() + 1
+    cache.save_state()
+    cache.volatile_data.clear()
+    cache.load_state()
+    assert cache.volatile_data[nid] ==  "test"
+    cache.clean_expire()
+    gevent.sleep(1)
+    assert "id" not in cache.volatile_data
+
+
+
+
+
